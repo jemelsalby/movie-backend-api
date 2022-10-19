6a92edabad4a6adbcb2da90b21355e23b8b68aa7
jemelsalby <jemelsalby3@gmail.com>
2022-10-20 02:35:58 +0530

    mba

---
+++ src/main/java/com/nestdigital/moviebackendapp/MovieBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.moviebackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBackendAppApplication.class, args);
+	}
+
+}
